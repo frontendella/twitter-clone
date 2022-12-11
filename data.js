184025d4b72b6da25fe20a0c3c606e609a7ab3f0
Ella R <ryukhinaella@gmail.com>
2022-12-10 22:33:21 -0800

    initial commit

---
+++ data.js
@@ -0,0 +1,57 @@
+export const tweetsData = [
+    {
+        handle: `@newUser 💎`,
+        profilePic: `images/user3.jpg`,
+        likes: 27,
+        retweets: 10,
+        tweetText: `Today is so beautiful day!`,
+        replies: [],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '4b161eee-c0f5-4545-9c4b-8562944223ee',
+    },
+    {
+        handle: `@onlyCode ✅`,
+        profilePic: `images/flower.jpg`,
+        likes: 6500,
+        retweets: 234,
+        tweetText: `What books can you recommend for the well-being?`,
+        replies: [
+            {
+                handle: `@numberOne ✅`,
+                profilePic: `images/tree.jpg`,
+                tweetText: `"At Your Best" by  Carey Nieuwhof`,
+            },
+            {
+                handle: `@ChuckNorris ✅`,
+                profilePic: `images/fox.jpg`,
+                tweetText: `Thursday Is the New Friday - very cool book, I recommend`,
+            },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '3c23454ee-c0f5-9g9g-9c4b-77835tgs2',
+    },
+    {
+        handle: `@NoobCoder12`,
+        profilePic: `images/user2.jpg `,
+        likes: 10,
+        retweets: 3,
+        tweetText: `Are you a coder if you only know HTML?`,
+        replies: [
+            {
+                handle: `@StackOverflower ☣️`,
+                profilePic: `images/green.jpg`,
+                tweetText: `No. Obviosuly not.`,
+            },
+            {
+                handle: `@YummyCoder64`,
+                profilePic: `images/ocean.jpg`,
+                tweetText: `You are wonderful just as you are! ❤️`,
+            },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '8hy671sff-c0f5-4545-9c4b-1237gyys45',
+    },
+]
